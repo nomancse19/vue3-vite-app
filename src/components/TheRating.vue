ed96ceb5c03dedc04302c57616047cac38905fec
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-14 21:51:26 +0600

    v-model two way data binding

---
+++ src/components/TheRating.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="rating">
+    <div
+      class="rating-star"
+      v-for="index in 5"
+      :key="index"
+      @click="handleClick(index)"
+    >
+      <img src="../img/star.png" alt="Rating Star" v-if="index <= modelValue" />
+      <img
+        src="../img/unstar.png"
+        alt="Rating Star"
+        v-if="index > modelValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Number,
+      default: 0,
+    },
+  },
+  //emits: ["update:modelValue"],
+  methods: {
+    handleClick(index) {
+      this.$emit("update:modelValue", index);
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.rating {
+  width: 200px;
+  display: flex;
+  align-items: center;
+}
+.rating-star {
+  margin: 2px;
+  cursor: pointer;
+}
+.rating-star img {
+  height: 40px;
+}
+.rating-star img:hover {
+  transform: scale(1.1);
+}
+</style>
