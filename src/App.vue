63bfd515aff3fb8b3a5955ce1804c43eaa34c97f
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-15 21:44:33 +0600

    Non-Prop Component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
 <template>
   <HelloWorld msg="Welcome To My Vue Js Site" class="hello"></HelloWorld>
   <div class="container">
-    <h2>Credit Card Info</h2>
-    <credit-card
-      v-model:nameOnCard="nameOnCard"
-      v-model:cardNumber="cardNumber"
-      v-model:expiry="expiry"
-      v-model:cvv="cvv"
-    ></credit-card>
+    <the-comment
+      v-model="myComment"
+      buttonText="Continue"
+      class="shadow"
+    ></the-comment>
     <br />
     <br />
     <br />
     <hr />
-    <p>Name On Card : {{ nameOnCard }} <br /></p>
-    <p>Card Number : {{ cardNumber }} <br /></p>
-    <p>Expiry Date: {{ expiry }} <br /></p>
-    <p>Card CVV : {{ cvv }} <br /></p>
+    <p>Your Comment is : {{ myComment }} <br /></p>
   </div>
 </template>
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import CreditCard from "./components/CreditCard.vue";
+import TheComment from "./components/TheComment.vue";
 export default {
   name: "App",
 
   data() {
     return {
-      nameOnCard: "Mr. ",
-      cardNumber: "",
-      expiry: "",
-      cvv: "",
+      myComment: "Mr. ",
     };
   },
   methods: {},
   components: {
     HelloWorld,
-    CreditCard,
+    TheComment,
   },
 };
 </script>
@@ -51,5 +43,11 @@ export default {
 }
 body {
   line-height: 1.8;
+}
+button {
+  padding: 5px 7px;
+}
+.shadow {
+  box-shadow: 0 0 3px black;
 }
 </style>
---
+++ src/components/TheComment.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="the-comment">
+    <textarea
+      :value="modelValue"
+      @input="handleInput"
+      id=""
+      cols="40"
+      rows="5"
+    ></textarea>
+    <div class="text-right">
+      <button>{{ buttonText }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    buttonText: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    handleInput(e) {
+      //console.log(e.target.value);
+      this.$emit("update:modelValue", e.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.the-comment {
+  width: 388px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  color: gray;
+  background-color: rgb(255, 254, 247);
+  margin-right: 150px;
+  padding: 35px;
+}
+input {
+  padding: 7px 9px;
+  border-radius: 3px;
+  width: 100%;
+  border: 1px solid gray;
+  margin-bottom: 11px;
+}
+.row {
+  display: flex;
+}
+.col-6 {
+  flex: 1;
+  padding: 0 2px;
+}
+.text-right {
+  text-align: right;
+}
+</style>
